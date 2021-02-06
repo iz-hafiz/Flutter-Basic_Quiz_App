62b4f9bab21d5f9547078e90608552f2e6d34b53
izzat-hafiz <67820671+izzat-hafiz@users.noreply.github.com>
2021-02-06 16:16:19 +0800

    Settle Lecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial_1/result.dart';
+
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyFirstApp());
 
 class MyFirstApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyFirstAppState();
+    return _MyFirstAppState();
   }
 }
 
-class MyFirstAppState extends State<MyFirstApp> {
-  var questionIndex = 0;
-  void answerQuestion() {
+class _MyFirstAppState extends State<MyFirstApp> {
+  //adding _anything cause it can't be acess by other dart file
+  final _questions = [
+    {
+      'questionText': 'what\'s your favourite colour?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 8},
+        {'text': 'Green', 'score': 14},
+        {'text': 'White', 'score': 13},
+      ],
+    },
+    {
+      'questionText': 'what\'s your favourite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 2},
+        {'text': 'Snake', 'score': 8},
+        {'text': 'Elephant', 'score': 6},
+        {'text': 'Lion', 'score': 5},
+      ],
+    },
+    {
+      'questionText': 'who\'s your favourite instructor?',
+      'answers': [
+        {'text': 'Sio', 'score': 3},
+        {'text': 'Mario', 'score': 10},
+        {'text': 'Blackio', 'score': 10},
+        {'text': 'Tio', 'score': 10},
+      ],
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {});
+    _questionIndex = 0;
+    _totalScore = 0;
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
-      questionIndex++;
-      if (questionIndex >= 2) {
-        questionIndex = 0;
-      }
+      _questionIndex++;
     });
-    print(questionIndex);
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'what\'s your favourite colour?',
-      'what\'s your favourite animal?',
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Column(
-          children: [
-            Text(questions[questionIndex]),
-            RaisedButton(
-              child: Text("Answer 1"),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text("Answer 2"),
-              onPressed: () => print("Answer 2 chosen!"),
-            ),
-            RaisedButton(
-                child: Text("Answer 3"),
-                onPressed: () {
-                  print("Answer 3 chosen!");
-                }),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
---
+++ lib/result.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    var resultText = 'You did it! \n';
+    if (resultScore <= 17) {
+      resultText += "Nice";
+    } else if (resultScore > 18) {
+      resultText += "Hmmm";
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      children: <Widget>[
+        Text(
+          resultPhrase,
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        FlatButton(
+          onPressed: resetHandler,
+          child: Text('Restart Quiz'),
+          textColor: Colors.blue,
+        )
+      ],
+    ));
+  }
+}
